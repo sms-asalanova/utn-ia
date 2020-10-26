6dee72be6990157ccaeb3ef7263e09137e496ab9
asalanova <asalanova@mulesoft.com>
2020-10-25 23:16:13 -0300

    Fix graph

--- src/main/java/com/utn/ia/com/utn/ia/jgap/Main.java
+++ src/main/java/com/utn/ia/com/utn/ia/jgap/Main.java
@@ -4,16 +4,24 @@ import com.utn.ia.model.Fixture;
 import com.utn.ia.model.Match;
 import com.utn.ia.utils.FixtureGeneratorFromTemplate;
 import org.jgap.*;
+import org.knowm.xchart.*;
+import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.colors.ChartColor;
+import org.knowm.xchart.style.colors.XChartSeriesColors;
+import org.knowm.xchart.style.lines.SeriesLines;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 
+import java.awt.*;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Set;
 
 public class Main {
 
-    private static final int MAX_EVOLUCIONES_PERMITIDAS = 400;
+    private static final int MAX_EVOLUCIONES_PERMITIDAS = 15;
     private static final int POBLACION_SIZE = 3000;
 
     public static void calularFixture() throws InvalidConfigurationException {
@@ -49,6 +57,10 @@ public class Main {
     private static PrintWriter printWriterValues;
     private static PrintWriter printWriterCsv;
     private static double lastBestFitness;
+    private static double[] yValues = new double[MAX_EVOLUCIONES_PERMITIDAS];
+    private static double[] xValues = new double[MAX_EVOLUCIONES_PERMITIDAS];
+
+
 
     private static void newIteration(String prefix, int i, IChromosome cromosomaMasApto) {
         try{
@@ -60,6 +72,8 @@ public class Main {
             printWriterCsv.print(Integer.toString(i) + ",");
             printWriterCsv.print(Double.toString(fit) + ",");
             printWriterCsv.print(cromosomaMasApto.getGenes()[0].getAllele().toString() + "\n");
+            yValues[i] = fit;
+            xValues[i] = i;
             if (fit < lastBestFitness){
                 System.out.println("Iteracion " + i + " :" + fit);
                 lastBestFitness = fit;
@@ -91,6 +105,18 @@ public class Main {
 
             printWriter.close();
             printWriterCsv.close();
+
+            // Create Chart
+            XYChart chart = new XYChartBuilder().width(800).height(600).title("Alg.Genetico").xAxisTitle("X").yAxisTitle("Y").build();
+
+            // generates linear data
+            XYSeries series = chart.addSeries("Fake Data", xValues, yValues);
+            series.setLineColor(XChartSeriesColors.BLUE);
+            series.setMarkerColor(Color.ORANGE);
+            series.setMarker(SeriesMarkers.CIRCLE);
+            series.setLineStyle(SeriesLines.SOLID);
+
+            BitmapEncoder.saveBitmap(chart, "./" + prefix + "_Chart",  BitmapEncoder.BitmapFormat.PNG);
         } catch (Exception e){
             throw new RuntimeException(e);
         }
